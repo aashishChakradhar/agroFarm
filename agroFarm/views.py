cb4e0cafae3d4fc71619681b4b3e1c865d093c8f
PR7175Z <aadimpraz@gmail.com>
2024-07-16 12:50:19 +0545

    Added function for dashboard view

--- agroFarm/views.py
+++ agroFarm/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect,HttpResponse
 from django.views import View
+from django.template import loader
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from django.urls import reverse_lazy
@@ -8,4 +9,8 @@ from agroFarm.models import *
 class Index(View):
     def get(self, request):
         return render(request,'index.html')
+    
+def dashboardpageloader(request):
+  template = loader.get_template('dashboard.html')
+  return HttpResponse(template.render())
         
