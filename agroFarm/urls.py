cf8b2a01c0593ef28d1a5150fcc7e5f10a93cd69
aashishChakradhar <ashishchakradhar01@gmail.com>
2024-07-16 15:42:16 +0545

    added color pallet and navbar

--- agroFarm/urls.py
+++ agroFarm/urls.py
@@ -4,5 +4,7 @@ from agroFarm import views
 
 urlpatterns = [
     #path("{urlpath}",{view class}, name="{reverse indexing}")
-    path('', views.Index.as_view(), name='index'),
+    path('', views.Index.as_view(), name='home'),
+    path('index',views.Index.as_view(),name='home'),
+    path('login', views.Login_view.as_view(), name='login'),
 ]
