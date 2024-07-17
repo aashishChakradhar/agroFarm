14ee1c04deae98e1d874a6f3ed23bcc5059aa606
Rajip2001 <rajiptuitui272@gmail.com>
2024-07-17 22:55:46 +0545

    footer added but not in permanent spot

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
