3995ae6512c7a7c649fc1e4be943a0faa136dd83
Kiran Khayamali <kirankhayamali@gmail.com>
2024-07-18 19:13:48 +0545

    loginPage and signupPage functions

--- agroFarm/views.py
+++ agroFarm/views.py
@@ -40,4 +40,38 @@ class Logout_view(View):
 def dashboardpageloader(request):
   template = loader.get_template('dashboard.html')
   return HttpResponse(template.render())
+
+
+#signup and login page ko lagi function from kiran
+@login_required(login_url = 'login')      
+def SignupPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if (password1 != password2):
+            return HttpResponse("Your password and confirm password doesn't match.")
+        else:
+            my_user = User.objects.create_user(username, email, password1)
+            my_user.save()
+            return redirect('login')
+    return render(request, 'signup.html')
+
+def LoginPage(request):
+    if request.method = 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, usename = usename, password = password)
+        if user is not None:
+            login(request, user)
+            customer = user.usename
+            return redirect('dashboard.') 
+        else:
+            return redirect('signup')
         
+    return render(request, 'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
