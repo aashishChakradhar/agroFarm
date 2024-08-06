eb811021ca2e9ab346432bf56c10e44ddba4b7cd
PR7175Z <aadimpraz@gmail.com>
2024-08-06 16:35:04 +0545

    Added url for media

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 admin.site.site_header = "AgroFarm Admin"
@@ -30,4 +32,4 @@ admin.site.index_title = "AgroFarm"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(('agroFarm.urls','agroFarm'),namespace = 'agroFarm')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
