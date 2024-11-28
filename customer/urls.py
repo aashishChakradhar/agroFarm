e7970d08a39b557abf0138849a80062e8c52f345
PR7175Z <aadimpraz@gmail.com>
2024-11-28 17:06:41 +0545

    Added search result

--- customer/urls.py
+++ customer/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('order-detail/', views.Order_Detail_View.as_view(), name='order-detail'),
     path('my-cart/', views.Cart_View.as_view(), name='my-cart'),
     path('add-data/', views.Automate_Data_Entry.as_view(), name='add-data'),
+    path('search/', views.search_product, name='search_products'),
     path('product-detail/<int:product_id>/', views.Product_Detail_View.as_view(), name='product-detail'),
 ]
