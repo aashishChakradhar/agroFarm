250d9ff709c972bd97d1d2dbfef85376896da02c
aashishChakradhar <ashishchakradhar00@gmail.com>
2024-11-28 20:52:16 +0545

    adding model for cart

--- merchant/models.py
+++ merchant/models.py
@@ -109,3 +109,10 @@ class PaymentMethod(BaseModel):
     is_online = models.BooleanField(default='False')
     def __str__(self):
         return self.orderID
+    
+class CartItem(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return f'{self.user} added {self.product} to cart'
