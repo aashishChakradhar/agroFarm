71119a4c97e7e4a74a150adf5b6cb299193e4e33
PR7175Z <aadimpraz@gmail.com>
2024-12-01 07:16:19 +0545

    Uncommented chartjs script

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,25 @@
-// const xValues = [50,60,70,80,90,100,110,120,130,140,150];
-// const yValues = [7,8,8,9,9,9,10,11,14,14,15];
+const xValues = [50,60,70,80,90,100,110,120,130,140,150];
+const yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-// new Chart("myChart", {
-//   type: "line",
-//   data: {
-//     labels: xValues,
-//     datasets: [{
-//       fill: false,
-//       lineTension: 0,
-//       backgroundColor: "rgba(0,0,255,1.0)",
-//       borderColor: "rgba(0,0,255,0.1)",
-//       data: yValues
-//     }]
-//   },
-//   options: {
-//     legend: {display: false},
-//     scales: {
-//       yAxes: [{ticks: {min: 6, max:16}}],
-//     }
-//   }
-// });
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
+    }
+  }
+});
 
 
 document.addEventListener("DOMContentLoaded", () => {
