a05de9e8b06ca508b270a865c8601fe887af1383
PR7175Z <aadimpraz@gmail.com>
2025-01-07 09:06:20 +0545

    Removed celery dependency

--- config/__init__.py
+++
@@ -1,8 +0,0 @@
-# config/__init__.py
-from __future__ import absolute_import, unicode_literals
-
-# Import celery
-from .celery import app as celery_app
-
-# Export celery app
-__all__ = ['celery_app']
