c76a231bad8d2778a3b6e5cffeb9424b2b41771f
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-01-07 19:35:35 +0545

    resolving confidentiality issue

--- config/settings.py
+++ config/settings.py
@@ -136,9 +136,13 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'collegeprojectwork404@gmail.com'  
-EMAIL_HOST_PASSWORD = 'ckix ungt mojr eugj'  
+#for email automation
+
+from decouple import config
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
