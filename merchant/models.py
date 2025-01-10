7e6259a68c49c2d26f24b3684f7e815c9e57e361
aashishChakradhar <ashishchakradhar00@gmail.com>
2025-01-10 16:43:51 +0545

    adding new attributes to the table

--- merchant/models.py
+++ merchant/models.py
@@ -62,7 +62,11 @@ class Product(BaseModel):
 
 class Product_User(BaseModel):
     userID = models.ForeignKey(User, on_delete=models.CASCADE)
-    produtID = models.ForeignKey(Product, on_delete=models.CASCADE)
+    productID = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+    review_average = models.DecimalField(max_digits=3, decimal_places=2)
     def __str__(self):
         return f"{self.userID} has {self.productID}"
     
