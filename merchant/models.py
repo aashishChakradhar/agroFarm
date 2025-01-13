3721005779fec9d9a97bab85e1e3e44bc1a24fd8
PR7175Z <aadimpraz@gmail.com>
2025-01-13 22:18:55 +0545

    Added latitude longitude in userdetail

--- merchant/models.py
+++ merchant/models.py
@@ -30,6 +30,8 @@ class ExtraUserDetails(BaseModel):
     mobile = PhoneNumberField()
     profileimg = models.CharField(max_length=1024, default='') 
     bio = models.CharField(max_length=1024, default='') 
+    latitude = models.DecimalField(max_digits=20, decimal_places=10, default=0.0)
+    longitude = models.DecimalField(max_digits=20, decimal_places=10, default=0.0)
     # addressID = models.OneToOneField(Address,on_delete=models.CASCADE, default=0)
     def __str__(self):
         return self.user.username
